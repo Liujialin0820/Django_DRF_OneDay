17b9a581a6e742cb8189d37b72c17267448d0b5f
jialin <mr.liujalo@gmail.com>
2024-12-24 17:40:50 +0800

    APIView 比View 构建了新的request对象 方便管理数据

--- CBV/views.py
+++ CBV/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import HttpResponse
-from django.views import View
+
+from rest_framework.views import APIView
 
 
-class BookView(View):
+# 构建了新的dispatch方法 取代之前的View
+# 功能一: 构建新的request对象, 让数据更好管理, 无论是urlencoded 还是json 都变成data:{}
+class BookView(APIView):
     def get(self, request):
-        return HttpResponse("View Get 请求")
+        print("request", request.query_params)
+        return HttpResponse("APIView Get 请求")
 
     def post(self, request):
-        return HttpResponse("Post 方法 ")
+
+        print("data", request.data)
+        return HttpResponse("APIView Post 方法 ")
