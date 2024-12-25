646b94d863f3fc73d5fef4cc4df1474f23ae6285
jialin <mr.liujalo@gmail.com>
2024-12-25 20:48:25 +0800

    ViewSet 的路由简化,router.register

--- settings/urls.py
+++ settings/urls.py
@@ -1,24 +1,31 @@
 from django.urls import path, re_path
 from sers import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register("sers/book", views.BookView)
+
 
 urlpatterns = [
-    path(
-        "sers/book/",
-        views.BookView.as_view(
-            {
-                "get": "list",
-                "post": "create",
-            }
-        ),
-    ),
-    re_path(
-        "sers/book/(?P<pk>\d+)",
-        views.BookView.as_view(
-            {
-                "get": "retrieve",
-                "put": "update",
-                "delete": "destroy",
-            }
-        ),
-    ),
+    #     path(
+    #         "sers/book/",
+    #         views.BookView.as_view(
+    #             {
+    #                 "get": "list",
+    #                 "post": "create",
+    #             }
+    #         ),
+    #     ),
+    #     re_path(
+    #         "sers/book/(?P<pk>\d+)",
+    #         views.BookView.as_view(
+    #             {
+    #                 "get": "retrieve",
+    #                 "put": "update",
+    #                 "delete": "destroy",
+    #             }
+    #         ),
+    #     ),
 ]
+
+urlpatterns += router.urls
